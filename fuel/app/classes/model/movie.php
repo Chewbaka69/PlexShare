5d7c8f0c9f2be5fe3f78591b2be17b14d52a78d0
Chewbaka69 <thejudoka69@msn.com>
2018-11-21 14:42:08 +0100

    Update movie get trailer

--- fuel/app/classes/model/movie.php
+++ fuel/app/classes/model/movie.php
@@ -414,43 +414,7 @@ class Model_Movie extends Model_Overwrite
 
     public function getTrailer()
     {
-        if($this->type === 'movie') {
-
-            $html = Request::forge('https://www.themoviedb.org/search/movie?query=' . urlencode($this->originalTitle ?: $this->title) . '+y%3A' . $this->year . '&language=us', 'curl');
-            $html->execute();
-
-            if ($html->response()->status !== 200)
-                return false;
-
-            $media = $html->response()->body;
-
-            preg_match('/<a id="[a-z0-9_]*" data-id="[a-z0-9]*" data-media-type="movie" data-media-adult="[a-z]*" class="[a-z]*" href="(\/movie\/[\d]*\?language\=us)" title=".*" alt=".*">/i', $media, $urls);
-
-            if (!isset($urls[1]))
-                return false;
-
-            $url = explode('?', $urls[1]);
-
-            $html = Request::forge('https://www.themoviedb.org' . $url[0] . '/videos?active_nav_item=Trailers&video_language=en-US&language=en-US', 'curl');
-            $html->set_options(array(
-                    CURLOPT_FOLLOWLOCATION => true,
-                )
-            );
-            $html->execute();
-
-            if ($html->response()->status !== 200)
-                return false;
-
-            $media = $html->response()->body;
-
-            preg_match('/<iframe type="text\/html" src="(\/\/www.youtube.com\/embed\/[a-zA-Z0-9\_]*\?enablejsapi\=1&autoplay\=0\&origin\=https%3A%2F%2Fwww\.themoviedb\.org\&hl\=en-US\&modestbranding\=1\&fs\=1)" frameborder\="0" allowfullscreen><\/iframe>/', $media, $youtube);
-
-            if (!isset($youtube[1]))
-                return false;
-
-            $youtube = preg_replace('/\&origin\=https%3A%2F%2Fwww\.themoviedb\.org/i', '', $youtube[1]);
-
-            $this->trailer = $youtube;
-        }
+        $trailer = new Model_Trailer($this->originalTitle ?: $this->title, $this->year, $this->type);
+        $this->trailer = $trailer->trailer;
     }
 }
