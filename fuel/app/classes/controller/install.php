e6ef080de4a96cc1affaddd95290c25d2f8195db
Chewbaka69 <thejudoka69@msn.com>
2018-12-03 10:24:23 +0100

    Fixes #5

--- fuel/app/classes/controller/install.php
+++ fuel/app/classes/controller/install.php
@@ -29,12 +29,12 @@ class Controller_Install extends Controller
         $config_db = Config::load('db', true);
         $config_db = $config_db['default'];
 
-        $view->set('db_host', $config_db['connection']['hostname']);
-        $view->set('db_port', $config_db['connection']['port']);
-        $view->set('db_database', $config_db['connection']['database']);
-        $view->set('db_prefix', $config_db['table_prefix']);
-        $view->set('db_username', $config_db['connection']['username']);
-        $view->set('db_password', $config_db['connection']['password']);
+        $view->set('db_host', isset($config_db['connection']['hostname']) ? $config_db['connection']['hostname'] : null);
+        $view->set('db_port', isset($config_db['connection']['port']) ? $config_db['connection']['port'] : null);
+        $view->set('db_database', isset($config_db['connection']['database']) ? $config_db['connection']['database'] : null);
+        $view->set('db_prefix', isset($config_db['table_prefix']) ? $config_db['table_prefix'] : null);
+        $view->set('db_username', isset($config_db['connection']['username']) ? $config_db['connection']['username'] : null);
+        $view->set('db_password', isset($config_db['connection']['password']) ? $config_db['connection']['password'] : null);
 
         try {
             $config_plex = Model_Server::find()[0];
